6715e9373127d34af02edb6935aec7caf8653247
kelvin-Linux <kelvinabache12@gmail.com>
2023-11-14 21:07:47 -0400

    estamos solucionando el problema, de comparacion entre una valor de consola y un string

--- control/condition.sh
+++ control/condition.sh
@@ -1,8 +1,10 @@
 read -p "Quieres usar en este proyecto? |y| |n|:" expression
 
-if (($expression == 'y'))
+if (($expression == "y" ))
   then
    echo "estas accetando la condicion"
    else 
     echo "estas rechasando la condicion"
 fi 
+
+#tenemos que usolucionar el problema de comparaccion entre un string y valor de consola 
