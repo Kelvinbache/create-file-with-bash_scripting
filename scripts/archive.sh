b9b708951fb35c336049e89049332608176d9272
kelvin-Linux <kelvinabache12@gmail.com>
2023-11-20 21:11:31 -0400

    intendo hacer una condicion que permita hacer comparaciones fuera de los archivos

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -5,3 +5,5 @@ read -p "name of archive:" archive
 > $file/$archive # crear un archivo en esta direccion
 
 ./control/condition.sh
+
+## nueva funcioalidad que nos permita, hacer una comprobacion a los archivos
