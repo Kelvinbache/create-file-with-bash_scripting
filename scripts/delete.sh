e270bc8d8bae91ffea163c46b456c3b3878eee61
kelvin abche <kelvinabache12@gmail.com>
2023-12-13 20:04:56 -0400

    haciendo algunas depuraciones al codigo, y intentar hacer una guia

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -5,9 +5,6 @@ read -p "Ponga la carpeta que quieres eliminar:" file
 if [ -d $file ] # estamos diceindo compprueba si el directorio existe 
   then 
       rm -r $file
-     elif [ -f $file ] # estamos diceindo comprueba si el archivo existe
-      then 
-       rm $file
    else  
      echo este nombre de archivo no existe en esta carpeta
 fi
